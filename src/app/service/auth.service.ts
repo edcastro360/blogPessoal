584ab3cc0ee84bd8eb684ffdcdc312cac660fc4d
Ed Castro <ed.castro360@gmail.com>
2021-06-01 22:33:47 -0300

    Componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -29,6 +30,16 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>("http://blogpessoal-heroku.herokuapp.com/usuarios/cadastrar", user)
     
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
