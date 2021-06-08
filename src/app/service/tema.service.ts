430b5079d0161b25ed3860738c463e02fab70fe7
Ed Castro <ed.castro360@gmail.com>
2021-06-08 12:40:35 -0300

    Implementação do put e delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,7 +1,7 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
 
 @Injectable({
@@ -19,8 +19,24 @@ export class TemaService {
     return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
 
 }
