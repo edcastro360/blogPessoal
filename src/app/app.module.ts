8de069208aebd5516058ecf3d0e9daa21f0f6510
Ed Castro <ed.castro360@gmail.com>
2021-06-10 13:57:46 -0300

    Implementando o put de user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 
 
 @NgModule({
@@ -31,6 +32,7 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     TemaDeleteComponent,
     PostagemEditComponent,
     PostagemDeleteComponent,
+    UserEditComponent,
     
   ],
   imports: [
